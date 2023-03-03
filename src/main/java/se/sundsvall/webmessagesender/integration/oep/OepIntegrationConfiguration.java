b5468d3045309882584dd555ec4355244c508fef
Joakim Schürer <joakim.schurer@sundsvall.se>
2023-03-03 13:11:50 +0100

    UF-4484: Acted on Sonatype's vulnerability findings

--- src/main/java/se/sundsvall/webmessagesender/integration/oep/OepIntegrationConfiguration.java
+++ src/main/java/se/sundsvall/webmessagesender/integration/oep/OepIntegrationConfiguration.java
@@ -9,7 +9,7 @@ import org.springframework.context.annotation.Import;
 import feign.auth.BasicAuthRequestInterceptor;
 import feign.jaxb.JAXBContextFactory;
 import feign.soap.SOAPDecoder;
-import feign.soap.SOAPEncoder.Builder;
+import feign.soap.SOAPEncoder;
 import feign.soap.SOAPErrorDecoder;
 import se.sundsvall.dept44.configuration.feign.FeignConfiguration;
 import se.sundsvall.dept44.configuration.feign.FeignMultiCustomizer;
@@ -19,7 +19,7 @@ public class OepIntegrationConfiguration {
 
 	private static final JAXBContextFactory JAXB_FACTORY = new JAXBContextFactory.Builder().build();
 
-	private static final Builder SOAP_ENCODER = new Builder()
+	private static final SOAPEncoder.Builder SOAP_ENCODER_BUILDER = new SOAPEncoder.Builder()
 		.withFormattedOutput(false)
 		.withJAXBContextFactory(JAXB_FACTORY)
 		.withSOAPProtocol(SOAPConstants.SOAP_1_1_PROTOCOL)
@@ -29,7 +29,7 @@ public class OepIntegrationConfiguration {
 	FeignBuilderCustomizer feignBuilderCustomizer(OepIntegrationProperties properties) {
 		return FeignMultiCustomizer.create()
 			.withDecoder(new SOAPDecoder(JAXB_FACTORY))
-			.withEncoder(SOAP_ENCODER.build())
+			.withEncoder(SOAP_ENCODER_BUILDER.build())
 			.withErrorDecoder(new SOAPErrorDecoder())
 			.withRequestInterceptor(new BasicAuthRequestInterceptor(properties.username(), properties.password()))
 			.withRequestTimeoutsInSeconds(properties.connectTimeout(), properties.readTimeout())
