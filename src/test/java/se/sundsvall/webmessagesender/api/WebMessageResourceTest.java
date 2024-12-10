34daa4abe8ccb740bf3215e7e87232b8a5cb4723
cheezi0747 <linus.sjolinder@b3.se>
2024-12-10 10:57:03 +0100

    UF-11274: Fix deperacted mockbean

--- src/test/java/se/sundsvall/webmessagesender/api/WebMessageResourceTest.java
+++ src/test/java/se/sundsvall/webmessagesender/api/WebMessageResourceTest.java
@@ -13,7 +13,7 @@ import java.util.UUID;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.reactive.server.WebTestClient;
 import se.sundsvall.webmessagesender.Application;
@@ -27,7 +27,7 @@ import se.sundsvall.webmessagesender.service.WebMessageService;
 @ActiveProfiles("junit")
 class WebMessageResourceTest {
 
-	@MockBean
+	@MockitoBean
 	private WebMessageService webMessageService;
 
 	@Autowired
