c531e921b8a791e46e2291ce349382149fb33e3d
cheezi0747 <linus.sjolinder@b3.se>
2024-12-17 09:35:55 +0100

    UF-11348: Add Circuitbreakers

--- src/main/java/se/sundsvall/webmessagesender/integration/oep/client/OepExternalClient.java
+++ src/main/java/se/sundsvall/webmessagesender/integration/oep/client/OepExternalClient.java
@@ -2,6 +2,7 @@ package se.sundsvall.webmessagesender.integration.oep.client;
 
 import static se.sundsvall.webmessagesender.integration.oep.config.OepExternalConfiguration.OEP_EXTERNAL_CLIENT;
 
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import org.springframework.cloud.openfeign.FeignClient;
 import se.sundsvall.webmessagesender.integration.oep.config.OepExternalConfiguration;
 
@@ -9,6 +10,7 @@ import se.sundsvall.webmessagesender.integration.oep.config.OepExternalConfigura
 	name = OEP_EXTERNAL_CLIENT,
 	url = "${integration.oep.external-url}",
 	configuration = OepExternalConfiguration.class)
+@CircuitBreaker(name = OEP_EXTERNAL_CLIENT)
 public interface OepExternalClient extends OepBaseClient {
 
 }
--- src/main/java/se/sundsvall/webmessagesender/integration/oep/client/OepInternalClient.java
+++ src/main/java/se/sundsvall/webmessagesender/integration/oep/client/OepInternalClient.java
@@ -2,6 +2,7 @@ package se.sundsvall.webmessagesender.integration.oep.client;
 
 import static se.sundsvall.webmessagesender.integration.oep.config.OepInternalConfiguration.OEP_INTERNAL_CLIENT;
 
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import org.springframework.cloud.openfeign.FeignClient;
 import se.sundsvall.webmessagesender.integration.oep.config.OepInternalConfiguration;
 
@@ -9,6 +10,7 @@ import se.sundsvall.webmessagesender.integration.oep.config.OepInternalConfigura
 	name = OEP_INTERNAL_CLIENT,
 	url = "${integration.oep.internal-url}",
 	configuration = OepInternalConfiguration.class)
+@CircuitBreaker(name = OEP_INTERNAL_CLIENT)
 public interface OepInternalClient extends OepBaseClient {
 
 }
