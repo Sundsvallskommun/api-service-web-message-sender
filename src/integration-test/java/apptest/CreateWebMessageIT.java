0655d899b151b812ac66c120aa35a25e9056e43a
Andreas Carlsson <andreas.carlsson@sundsvall.se>
2025-01-21 14:12:38 +0100

    UF-11684: Updated IT-tests

--- src/integration-test/java/apptest/CreateWebMessageIT.java
+++ src/integration-test/java/apptest/CreateWebMessageIT.java
@@ -1,22 +1,18 @@
 package apptest;
 
-import static org.assertj.core.api.Assertions.assertThat;
 import static org.springframework.http.HttpHeaders.LOCATION;
+import static org.springframework.http.HttpMethod.GET;
 import static org.springframework.http.HttpMethod.POST;
 import static org.springframework.http.HttpStatus.CREATED;
 import static org.springframework.http.HttpStatus.INTERNAL_SERVER_ERROR;
 import static org.springframework.http.HttpStatus.NOT_FOUND;
+import static org.springframework.http.HttpStatus.OK;
 
 import java.util.List;
-
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-
 import se.sundsvall.dept44.test.AbstractAppTest;
 import se.sundsvall.dept44.test.annotation.wiremock.WireMockAppTestSuite;
 import se.sundsvall.webmessagesender.Application;
-import se.sundsvall.webmessagesender.integration.db.WebMessageRepository;
-import se.sundsvall.webmessagesender.integration.db.model.WebMessageEntity;
 
 @WireMockAppTestSuite(
 	files = "classpath:/CreateWebMessage/",
@@ -28,26 +24,24 @@ class CreateWebMessageIT extends AbstractAppTest {
 	private static final String REQUEST_FILE = "request.json";
 	private static final String RESPONSE_FILE = "response.json";
 
-	@Autowired
-	private WebMessageRepository repository;
-
 	@Test
 	void test1_createInternalMessageSuccessful() {
-		setupCall()
+
+		final var headers = setupCall()
 			.withServicePath(PATH)
 			.withHttpMethod(POST)
 			.withRequest(REQUEST_FILE)
 			.withExpectedResponseStatus(CREATED)
 			.withExpectedResponseHeader(LOCATION, List.of("^/2281/webmessages(.*)$"))
-			.sendRequestAndVerifyResponse();
+			.sendRequest()
+			.getResponseHeaders();
 
-		final var webMessages = repository.findByMunicipalityIdAndExternalReferencesKeyAndExternalReferencesValueOrderByCreated(MUNICIPALITY_ID, "flowInstanceId", "356434");
-		assertThat(webMessages).hasSize(1);
-		assertThat(webMessages.getFirst()).satisfies(webMessage -> {
-			assertThat(webMessage.getOepInstance()).isEqualTo("internal");
-			assertThat(webMessage.getPartyId()).isEqualTo("81471222-5798-11e9-ae24-57fa13b361e1");
-			assertThat(webMessage.getMessage()).isEqualTo("This is a message");
-		});
+		setupCall()
+			.withServicePath(headers.get(LOCATION).stream().findFirst().get())
+			.withHttpMethod(GET)
+			.withExpectedResponseStatus(OK)
+			.withExpectedResponse(RESPONSE_FILE)
+			.sendRequestAndVerifyResponse();
 	}
 
 	@Test
@@ -74,48 +68,59 @@ class CreateWebMessageIT extends AbstractAppTest {
 
 	@Test
 	void test4_createWithAttachmentSuccessful() {
-		setupCall()
+		final var headers = setupCall()
 			.withServicePath(PATH)
 			.withHttpMethod(POST)
 			.withRequest(REQUEST_FILE)
 			.withExpectedResponseStatus(CREATED)
 			.withExpectedResponseHeader(LOCATION, List.of("^/2281/webmessages/(.*)$"))
-			.sendRequestAndVerifyResponse();
+			.sendRequest()
+			.getResponseHeaders();
 
-		assertThat(repository.findByMunicipalityIdAndExternalReferencesKeyAndExternalReferencesValueOrderByCreated(MUNICIPALITY_ID, "flowInstanceId", "2154"))
-			.extracting(WebMessageEntity::getAttachments).hasSize(1);
+		setupCall()
+			.withServicePath(headers.get(LOCATION).stream().findFirst().get())
+			.withHttpMethod(GET)
+			.withExpectedResponseStatus(OK)
+			.withExpectedResponse(RESPONSE_FILE)
+			.sendRequestAndVerifyResponse();
 	}
 
 	@Test
 	void test5_createExternalMessageSuccessful() {
-		setupCall()
+		final var headers = setupCall()
 			.withServicePath(PATH)
 			.withHttpMethod(POST)
 			.withRequest(REQUEST_FILE)
 			.withExpectedResponseStatus(CREATED)
 			.withExpectedResponseHeader(LOCATION, List.of("^/2281/webmessages/(.*)$"))
-			.sendRequestAndVerifyResponse();
+			.sendRequest()
+			.getResponseHeaders();
 
-		final var webMessages = repository.findByMunicipalityIdAndExternalReferencesKeyAndExternalReferencesValueOrderByCreated(MUNICIPALITY_ID, "flowInstanceId", "356435");
-		assertThat(webMessages).hasSize(1);
+		setupCall()
+			.withServicePath(headers.get(LOCATION).stream().findFirst().get())
+			.withHttpMethod(GET)
+			.withExpectedResponseStatus(OK)
+			.withExpectedResponse(RESPONSE_FILE)
+			.sendRequestAndVerifyResponse();
 	}
 
 	@Test
 	void test6_createMessageWithoutPartyId() {
-		setupCall()
+
+		final var headers = setupCall()
 			.withServicePath(PATH)
 			.withHttpMethod(POST)
 			.withRequest(REQUEST_FILE)
 			.withExpectedResponseStatus(CREATED)
 			.withExpectedResponseHeader(LOCATION, List.of("^/2281/webmessages(.*)$"))
-			.sendRequestAndVerifyResponse();
+			.sendRequest()
+			.getResponseHeaders();
 
-		final var webMessages = repository.findByMunicipalityIdAndExternalReferencesKeyAndExternalReferencesValueOrderByCreated(MUNICIPALITY_ID, "flowInstanceId", "356436");
-		assertThat(webMessages).hasSize(1);
-		assertThat(webMessages.getFirst()).satisfies(webMessage -> {
-			assertThat(webMessage.getOepInstance()).isEqualTo("external");
-			assertThat(webMessage.getPartyId()).isNull();
-			assertThat(webMessage.getMessage()).isEqualTo("This is a message");
-		});
+		setupCall()
+			.withServicePath(headers.get(LOCATION).stream().findFirst().get())
+			.withHttpMethod(GET)
+			.withExpectedResponseStatus(OK)
+			.withExpectedResponse(RESPONSE_FILE)
+			.sendRequestAndVerifyResponse();
 	}
 }
